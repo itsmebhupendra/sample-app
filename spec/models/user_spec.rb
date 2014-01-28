89f4055a2694650eb78c4020ab30aa1fe17c6b36
Bhupendra <bhupendra.singh@rsystems.com>
2014-01-28 13:00:31 +0100

    Email validation with case sensitivity added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -91,5 +91,14 @@ describe User do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
   #has_secure_password
 end
