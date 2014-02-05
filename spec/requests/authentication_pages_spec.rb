5034a3bd094af65611195fe0a15ba15697a7482d
Bhupendra <bhupendra.singh@rsystems.com>
2014-02-05 07:27:13 +0100

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -47,6 +47,17 @@ describe "Authentication" do
     end
   end
   describe "authorization" do
+   describe "as non-admin user" do
+     let(:user) { FactoryGirl.create(:user) }
+     let(:non_admin) { FactoryGirl.create(:user) }
+
+     before { sign_in non_admin, no_capybara: true }
+
+     describe "submitting a DELETE request to the Users#destroy action" do
+       before { delete user_path(user) }
+       specify { expect(response).to redirect_to(root_url) }
+     end
+   end
 
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
@@ -57,7 +68,6 @@ describe "Authentication" do
           fill_in "Password", with: user.password
           click_button "Sign in"
         end
-
         describe "after signing in" do
 
           it "should render the desired protected page" do
